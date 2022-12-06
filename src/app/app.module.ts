16774c39937cf37ab421427215b370261fb26f51
sufailbadushat <sufailbadushat@gamil.com>
2022-12-06 23:14:39 +0530

    view product tab add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddProductComponent,
-    NavBarComponent
+    NavBarComponent,
+    ViewProductComponent
   ],
   imports: [
     BrowserModule,
