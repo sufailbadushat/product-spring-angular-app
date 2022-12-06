3db122a498f3c29b1aff5aa88f122bfbd868b6f6
sufailbadushat <sufailbadushat@gamil.com>
2022-12-06 23:04:25 +0530

    navigation bar add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddProductComponent
+    AddProductComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
